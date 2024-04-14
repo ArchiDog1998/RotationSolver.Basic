c4c9fb399bf1f1624e5a246c44d73e7143498abd
秋水 <53346444+ArchiDog1998@users.noreply.github.com>
2024-04-14 11:07:14 +0800

    Add project files.

---
+++ RotationSolver.Basic/Attributes/RotationConfigAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace RotationSolver.Basic.Attributes;
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="type"></param>
+[AttributeUsage(AttributeTargets.Property)]
+public class RotationConfigAttribute(CombatType type) : Attribute
+{
+    /// <summary>
+    /// The type of this config.
+    /// </summary>
+    public CombatType Type => type;
+}
---
+++ RotationSolver.Basic/Rotations/Duties/FF16Rotation.cs
@@ -0,0 +1,19 @@
+﻿
+namespace RotationSolver.Basic.Rotations.Duties;
+
+/// <summary>
+/// The Memory of Embers.
+/// </summary>
+
+[DutyTerritory(1166)]
+public abstract class FF16Rotation : DutyRotation
+{
+}
+
+partial class DutyRotation
+{
+    //static partial void ModifyPrecisionStrikePvE(ref ActionSetting setting)
+    //{
+    //    setting.StatusNeed = [StatusID.Precision];
+    //}
+}
