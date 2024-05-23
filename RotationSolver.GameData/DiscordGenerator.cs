9b8eedfaf2d11bd3f667d1a20b0957ad31b4366e
秋水 <53346444+ArchiDog1998@users.noreply.github.com>
2024-05-23 21:33:52 +0800

    fix: for the dc bot data.

--- RotationSolver.GameData/DiscordGenerator.cs
+++ RotationSolver.GameData/DiscordGenerator.cs
@@ -6,21 +6,22 @@ internal static class DiscordGenerator
     {
         var dir = dirInfo.Parent!.Parent!.FullName + "\\RotationSolver.DiscordBot\\RotationSolver.DiscordBot\\Resource.cs";
 
-        var dict = new Dictionary<string, uint>();
+        var dict = new Dictionary<string, ContentFinderCondition>();
         foreach (var item in gameData.GetExcelSheet<ContentFinderCondition>()!
+            .Where(i => i.Image != 0)
             .Where(i => !string.IsNullOrEmpty(i.Name)))
         {
-            dict[item.Name] = item.Image;
+            dict[item.Name] = item;
         }
 
-        var items = dict.Select(i => $"        (\"{i.Key}\", {i.Value}),");
+        var items = dict.Select(i => $"        (\"{i.Key}\", {i.Value.Image}, {i.Value.Content}),");
 
         var code = $$"""
          namespace RotationSolver.DiscordBot;
 
          internal static class Resource
          {
-             public static readonly List<(string, uint)> DutyAndImage =
+             public static readonly List<(string, uint, ushort)> DutyAndImage =
              [
          {{string.Join("\n", items)}}
              ];
