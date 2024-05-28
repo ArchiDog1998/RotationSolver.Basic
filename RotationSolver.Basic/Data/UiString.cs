56ff04707d0d63ace96564531bcb1a1caf08e921
秋水 <53346444+ArchiDog1998@users.noreply.github.com>
2024-05-28 23:21:28 +0800

    docs: desc.

--- RotationSolver.Basic/Data/UiString.cs
+++ RotationSolver.Basic/Data/UiString.cs
@@ -803,7 +803,7 @@ internal enum UiString
     [Description("The condition boolean is available only for supporters!")]
     CantUseConditionBoolean,
 
-    [Description("The duty {0} can only be farmed for supporters! Please do it next day!")]
+    [Description("The duty {0} can only be farmed by supporters! Please do it next day!")]
     CantFarmThisDuty,
 
     [Description("This feature is available for supporter! You can modify it, but it will not take effect.")]
