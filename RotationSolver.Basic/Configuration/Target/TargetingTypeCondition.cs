526f748884e955c59291e9c1198422d7cb64df43
秋水 <1123993881@qq.com>
2024-06-17 10:05:53 +0800

    fix: add targeting type condition.

---
+++ RotationSolver.Basic/Configuration/Target/TargetingTypeCondition.cs
@@ -0,0 +1,35 @@
+﻿using Dalamud.Game.ClientState.Objects.SubKinds;
+using ECommons.GameHelpers;
+using XIVConfigUI.Attributes;
+
+namespace RotationSolver.Basic.Configuration.Target;
+
+[Description("Type Condition")]
+
+internal class TargetingTypeCondition : TargetingConditionBase
+{
+    [UI("Type")]
+    public TargetingType TargetingType { get; set; } = TargetingType.MySelf;
+
+    public override bool IsTrue(GameObject obj)
+    {
+        return TargetingType switch
+        {
+             TargetingType.MySelf => obj.ObjectId == Player.Object.ObjectId,
+             TargetingType.Player => obj is PlayerCharacter,
+             TargetingType.Battle => obj is BattleChara,
+             TargetingType.Hostile => obj is BattleChara battle && battle.IsEnemy(),
+             TargetingType.Fiendly => obj is BattleChara battle && battle.IsAlliance(),
+            _ => false,
+        };
+    }
+}
+
+internal enum TargetingType : byte
+{
+    MySelf,
+    Player,
+    Battle,
+    Hostile,
+    Fiendly,
+}
